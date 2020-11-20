2393846344ae13b7811dccc4948a5a5f7d5cc405
viper75 <sean.huvaya03@yahoo.com>
2020-11-20 22:41:48 +0200

    Set notification content

--- app/src/main/java/org/viper75/android_notifications/MainActivity.java
+++ app/src/main/java/org/viper75/android_notifications/MainActivity.java
@@ -1,6 +1,7 @@
 package org.viper75.android_notifications;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
 
 import android.os.Bundle;
 
@@ -10,5 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
+                .setSmallIcon(R.drawable.notification_icon)
+                .setContentTitle("Basic Notification")
+                .setContentText("Basic notification example to demonstrate how it works")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
     }
 }
