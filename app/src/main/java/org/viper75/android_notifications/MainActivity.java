d6f0ad1d3f95e1a710397f0e471c266ed86c4ae7
viper75 <sean.huvaya03@yahoo.com>
2020-11-20 22:52:11 +0200

    Create notification channel and set the importance

--- app/src/main/java/org/viper75/android_notifications/MainActivity.java
+++ app/src/main/java/org/viper75/android_notifications/MainActivity.java
@@ -3,9 +3,14 @@ package org.viper75.android_notifications;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.NotificationCompat;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.os.Build;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String CHANNEL_ID = "My Channel";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +22,17 @@ public class MainActivity extends AppCompatActivity {
                 .setContentTitle("Basic Notification")
                 .setContentText("Basic notification example to demonstrate how it works")
                 .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+    }
+
+    public void createNotificationChannel() {
+        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.O) {
+            CharSequence name = getString(R.string.notification_channel_name);
+            String description = getString(R.string.notification_channel_description);
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+            channel.setDescription(description);
+            NotificationManager manager = getSystemService(NotificationManager.class);
+            manager.createNotificationChannel(channel);
+        }
     }
 }
